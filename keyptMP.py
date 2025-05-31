949110c30b489c28b1df25b9567db4a745b3ac24
bluffering <eqflvrs@gmail.com>
2025-05-31 12:31:01 -0700

    i think i am going insane

--- keyptMP.py
+++ keyptMP.py
@@ -72,14 +72,14 @@ for source_path in glob('Videos/*'):
                     landmarks = results.pose_landmarks.landmark
 
                     def calculate_angle(a,b,c):
-                        a = np.array(a) # First
-                        b = np.array(b) # Mid
-                        c = np.array(c) # End
+                        a = np.array(a)
+                        b = np.array(b)
+                        c = np.array(c)
     
                         radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
                         angle = np.abs(radians*180.0/np.pi)
     
-                        if angle >180.0:
+                        if angle > 180.0:
                             angle = 360-angle
         
                         return angle 
